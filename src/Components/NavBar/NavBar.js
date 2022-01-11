5e96f1018b815264ae2d3957a0ff6b04db7af55b
Jscaporaso <sebastiancaporaso@gmail.com>
2022-01-11 14:52:54 -0300

    Clase 4

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import "./NavBar.css";
+
+function NavBar() {
+    return <nav>
+        <header class="header">
+            <nav class="header_nav">
+                <a class="nav_enlace" href="#locales">Locales</a>
+                <a class="nav_enlace" href="#contacto">Contacto</a>
+                <a class="nav_enlace" href="#seguinos">Seguinos</a>
+            </nav>
+        </header>
+    </nav>
+}
+export default NavBar;
